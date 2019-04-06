dc9a9399418526a715f63c3c7fab7ccf0f4cff15
francisco-aguilar <r.franciscoaguilar@gmail.com>
2019-04-06 11:29:17 -0300

    agrego cosas

--- src/main/java/proyecto/Auto.java
+++ src/main/java/proyecto/Auto.java
@@ -1,21 +1,44 @@
 package proyecto;
 
 public class Auto {
-	int combustible;
+	int capacidadCombustible;
 	boolean encendido;
 	double temperatura;
+	int velocidad;
+	int combustible = capacidadCombustible;
 	
-	public Auto(int combustible) {
-		this.combustible = combustible;
+	public Auto(int capacidadCombustible) {
+		this.capacidadCombustible = capacidadCombustible;
+	}
+	
+	public double getTemperatura() {
+		return temperatura;
+	}
+	
+	public void detener() {
+		velocidad = 0;
 	}
 	
 	public void encender() {
 		encendido = true;
+		this.aumentarTemperatura(70);
 	}
 	
 	public void apagar() {
 		encendido = false;
 		temperatura = 0;
+	}
+	
+	public void aumentarTemperatura(double aumento) {
+		temperatura += aumento;
+	}
+	
+	public void consumirCombustible(double consumo) {
+		combustible -= consumo * combustible;
+	}
+	
+	public double porcentajeCombustibleActual() {
+		return combustible * 100 / capacidadCombustible;
 	}
 	
 	
